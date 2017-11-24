8223276f6b7cc83a4e8add371eba1894b6bf079e
hbjm471754856 <javawebwork@163.com>
2017-11-25 02:35:20 +0800

    今日学习@ConfigurationProperties注解的用法，主要是在java文件编译为class文件时执行该类，为application.properties文件提供属性约束

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,20 @@
 package com.example;
 
+import com.example.util.SpringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        System.out.println("<<<<<<<<<<<<<<<<<<<<Spring Boot开始启动>>>>>>>>>>>>>>>>>>>>>");
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("<<<<<<<<<<<<<<<<<<<<Spring Boot启动成功>>>>>>>>>>>>>>>>>>>>>");
+        DemoProperties demoProperties = SpringUtils.getBean("demoProperties");
+        System.out.println("自定义属性文件获取值，username=" + demoProperties.getUsername());
+    }
+
+
 }
---
+++ src/main/java/com/example/DemoProperties.java
@@ -0,0 +1,22 @@
+package com.example;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+//该类为spring容器组建
+@Component
+//编译时运行加载该类，约束application.properties文件中的属性
+@ConfigurationProperties(prefix = "com.example")
+public class DemoProperties {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
